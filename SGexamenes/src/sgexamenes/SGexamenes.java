520619e498824665809e520d997aefcdcd3cee4f
Frarlon Rodriguez <fr0609@gmail.com>
2013-10-18 22:54:51 -0600

    Creacion del Projecto SGExamenes

---
+++ SGexamenes/src/sgexamenes/SGexamenes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sgexamenes;
+
+/**
+ *
+ * @author fr0609
+ */
+public class SGexamenes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
